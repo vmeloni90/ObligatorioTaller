823af4315f05e173b980990ccaf34f86ffaca1f4
user <user@192.168.1.8>
2023-02-16 18:35:59 -0300

    agregue utils.js y mod archivos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,99 @@
-document
-  .querySelector("ion-router")
-  .addEventListener("ionRouteDidChange", manejarRuta);
+const baseUrl = "https://dwallet.develotion.com/";
+let token = obtenerSesionUsuario();
+
+const $ = {};
+
+iniciarApp();
+function iniciarApp() {
+  guardarElementos();
+  agregarEventos();
+}
+function guardarElementos() {
+  $.ionRouter = document.querySelector("ion-router");
+  $.formRegistro = document.querySelector("#formRegistroUsuario");
+  $.formLogin = document.querySelector("#formLoginUsuario");
+}
+
+function agregarEventos() {
+  $.ionRouter.addEventListener("ionRouteDidChange", manejarRuta);
+  $.formRegistro.addEventListener("submit", manejarRegistroUsuario);
+  $.formLogin.addEventListener("submit", manejarLoginUsuario);
+}
+
+function manejarRegistroUsuario(event) {
+  event.preventDefault();
+
+  const datos = obtenerDatosRegistro();
+  console.log("registro", datos);
+
+  registrarUsuario(datos);
+}
+
+function registrarUsuario(usuario) {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  const data = {
+    usuario: usuario.usuario,
+    password: usuario.password,
+    departamento: usuario.departamento,
+    ciudad: usuario.ciudad,
+    
+  };
+
+  fetch(`${baseUrl}/usuarios`, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(data),
+  })
+    .then(function (rawResponse) {
+      return rawResponse.json();
+    })
+    .then(function (jsonReposponse) {
+      console.log("then registro", jsonReposponse);
+      if (jsonReposponse.error === "") {
+        navegar("/login");
+      } else {
+        throw jsonReposponse.error;
+      }
+    })
+    .catch(function (error) {
+      console.warn(error);
+    });
+}
+
+function navegar(path) {
+  $.ionRouter.push(path);
+}
+
+function manejarLoginUsuario(event) {
+  event.preventDefault();
+
+  const datos = obtenerDatosLogin();
+  console.log("login", datos);
+
+  // Validamos
+
+  loginUsuario(datos);
+}
+
+function obtenerDatosRegistro() {
+  return {
+    usuario: $.formRegistro.querySelector("#inpUsuario").value,
+    password: $.formRegistro.querySelector("#inpPassword").value,
+    departamento: $.formRegistro.querySelector("#selDepartamento").value,
+    ciudad: $.formRegistro.querySelector("#selCiudad").value,
+    
+  };
+}
+function obtenerDatosLogin() {
+  return {
+    email: $.formLogin.querySelector("#inp_usuario").value,
+    password: $.formLogin.querySelector("#inp_password").value,
+  };
+}
+
 
 function manejarRuta(event) {
   const path = event.detail.to;
@@ -9,15 +102,47 @@ function manejarRuta(event) {
   ocultarPageActiva();
   switch (path) {
     case "/":
-      document.querySelector("#page-home").classList.add("page-activa");
+      mostrarPagina("#page-home");
       break;
     case "/registro":
-      document.querySelector("#page-registro").classList.add("page-activa");
+      mostrarPagina("#page-registro");
       break;
     case "/login":
-      document.querySelector("#page-login").classList.add("page-activa");
+      mostrarPagina("#page-login");
       break;
   }
+}
+
+function loginUsuario(usuario) {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  const data = {
+    email: usuario.email,
+    password: usuario.password,
+  };
+
+  fetch(`${baseUrl}/usuarios/session`, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(data),
+  })
+    .then(function (rawResponse) {
+      return rawResponse.json();
+    })
+    .then(function (jsonReposponse) {
+      console.log("then login", jsonReposponse);
+      if (jsonReposponse.error === "") {
+        guardarSesionUsuario(jsonReposponse.data.token);
+        navegar("/");
+      } else {
+        throw jsonReposponse.error;
+      }
+    })
+    .catch(function (error) {
+      console.warn(error);
+    });
 }
 
 function ocultarPageActiva() {
@@ -25,6 +150,10 @@ function ocultarPageActiva() {
   if ($pageActive) {
     $pageActive.classList.remove("page-activa");
   }
+}
+
+function mostrarPagina(id) {
+  document.querySelector(id).classList.add("page-activa");
 }
 
 // Ocultar y mostrar el password
@@ -41,3 +170,7 @@ document.getElementById("mostrar_pwd").onclick = function () {
     document.getElementById("inp_password").type = "text";
   }
 };
+
+function obtenerSesionUsuario() {
+  return leerLocalStorage("tokenUsuario", "");
+}
---
+++ js/utils.js
@@ -0,0 +1,20 @@
+function crearUrl(baseUrl, params) {
+    const urlObj = new URL(baseUrl);
+    urlObj.search = new URLSearchParams(params).toString();
+    return urlObj.href;
+  }
+  
+  function guardarLocalStorage(clave, valor) {
+    localStorage.setItem(clave, JSON.stringify(valor));
+  }
+  
+  function leerLocalStorage(clave, valorPorDefecto) {
+    const valorStorage = JSON.parse(localStorage.getItem(clave));
+  
+    if (valorStorage === null) {
+      return valorPorDefecto;
+    } else {
+      return valorStorage;
+    }
+  }
+  
